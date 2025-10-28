ab28bcab2b2b5f4fc89686ae3afce1f2f2e15538
Ashour Malik Ismail <ashourmalikismail@MacBook-Air-som-tillhor-Ashour.local>
2025-10-28 08:42:13 +0100

    Skapade add medlemmar sida

--- src/main/java/com/HelloUser/HelloUser/AddMedlemmar.java
+++ src/main/java/com/HelloUser/HelloUser/AddMedlemmar.java
@@ -1,13 +1,13 @@
 package com.HelloUser.HelloUser;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class Valkommen_sida{
+public class AddMedlemmar {
+
+    @GetMapping("/AddMedlemmar")
+    public String AddMedlemmar() {
+        return "AddMedlemmar";
+    }
     
-@GetMapping("/")
-public String valkommen() {
-    return "index";
-}
 }
--- src/main/resources/static/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("adminBtn").addEventListener("click", function() {
-    console.log("Admin klickad");
-});
-
-document.getElementById("membersBtn").addEventListener("click", function() {
-    console.log("Medlemmar klickad");
-});
