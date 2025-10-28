ec2c53cc194824acc8ef301d7f5e6aeaabc8801e
Ashour Malik Ismail <ashourmalikismail@MacBook-Air-som-tillhor-Ashour.local>
2025-10-28 17:06:03 +0100

    Skapade post metod

--- src/main/java/com/HelloUser/HelloUser/AddMedlemmar.java
+++ src/main/java/com/HelloUser/HelloUser/AddMedlemmar.java
@@ -1,21 +1,29 @@
 package com.HelloUser.HelloUser;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class AddMedlemmar {
 
-    @GetMapping("/AddMedlemmar")
-    public String AddMedlemmar() {
-        return "AddMedlemmar";
-    }
-    
-
    
 
-    
-    
+    @GetMapping("/AddMedlemmar")
+    public String showAddMedlemmarForm(Model model) {
+        model.addAttribute("medlem", new Medlemmar());
+        return "AddMedlemmar";
+    }
+
+   @PostMapping("/AddMedlemmar")
+    public String addMedlemmar(Medlemmar medlem, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("message", "Medlem tillagd framgångsrikt!");
+        System.out.println("Medlem tillagd: " + medlem.getFirstname() + " " + medlem.getLastname() + ", Födelsedag: " + medlem.getBirthdate() + ", E-post: " + medlem.getEmail());
+        return "redirect:/AddMedlemmar";
+    }
+
 }
+    
+
